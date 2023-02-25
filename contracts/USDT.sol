1728da16f55c3adf65b9a5ddf441fb3c578343c9
10dimensions <relay.10d@gmail.com>
2023-02-25 15:47:28 +0000

    Fix USDT compile issues

--- contracts/USDT.sol
+++ contracts/USDT.sol
@@ -49,7 +49,7 @@ contract Ownable {
       * @dev The Ownable constructor sets the original `owner` of the contract to the sender
       * account.
       */
-    function Ownable() public {
+    constructor() public {
         owner = msg.sender;
     }
 
@@ -78,11 +78,11 @@ contract Ownable {
  * @dev Simpler version of ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
-contract ERC20Basic {
+abstract contract ERC20Basic {
     uint public _totalSupply;
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address who) public constant returns (uint);
-    function transfer(address to, uint value) public;
+    function totalSupply() public view virtual returns (uint);
+    function balanceOf(address who) public view virtual returns (uint);
+    function transfer(address to, uint value) public virtual;
     event Transfer(address indexed from, address indexed to, uint value);
 }
 
@@ -90,10 +90,10 @@ contract ERC20Basic {
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
-contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) public constant returns (uint);
-    function transferFrom(address from, address to, uint value) public;
-    function approve(address spender, uint value) public;
+abstract contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender) public view virtual returns (uint);
+    function transferFrom(address from, address to, uint value) public virtual;
+    function approve(address spender, uint value) public virtual;
     event Approval(address indexed owner, address indexed spender, uint value);
 }
 
@@ -101,7 +101,7 @@ contract ERC20 is ERC20Basic {
  * @title Basic token
  * @dev Basic version of StandardToken, with no allowances.
  */
-contract BasicToken is Ownable, ERC20Basic {
+abstract contract BasicToken is Ownable, ERC20Basic {
     using SafeMath for uint;
 
     mapping(address => uint) public balances;
@@ -123,7 +123,7 @@ contract BasicToken is Ownable, ERC20Basic {
     * @param _to The address to transfer to.
     * @param _value The amount to be transferred.
     */
-    function transfer(address _to, uint _value) public onlyPayloadSize(2 * 32) {
+    function transfer(address _to, uint _value) public virtual override onlyPayloadSize(2 * 32) {
         uint fee = (_value.mul(basisPointsRate)).div(10000);
         if (fee > maximumFee) {
             fee = maximumFee;
@@ -133,17 +133,16 @@ contract BasicToken is Ownable, ERC20Basic {
         balances[_to] = balances[_to].add(sendAmount);
         if (fee > 0) {
             balances[owner] = balances[owner].add(fee);
-            Transfer(msg.sender, owner, fee);
+            emit Transfer(msg.sender, owner, fee);
         }
-        Transfer(msg.sender, _to, sendAmount);
+        emit Transfer(msg.sender, _to, sendAmount);
     }
 
     /**
     * @dev Gets the balance of the specified address.
     * @param _owner The address to query the the balance of.
-    * @return An uint representing the amount owned by the passed address.
     */
-    function balanceOf(address _owner) public constant returns (uint balance) {
+    function balanceOf(address _owner) public view virtual override returns (uint balance) {
         return balances[_owner];
     }
 
@@ -156,8 +155,8 @@ contract BasicToken is Ownable, ERC20Basic {
  * @dev https://github.com/ethereum/EIPs/issues/20
  * @dev Based oncode by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
  */
-contract StandardToken is BasicToken, ERC20 {
-
+abstract contract StandardToken is BasicToken, ERC20 {
+    using SafeMath for uint;
     mapping (address => mapping (address => uint)) public allowed;
 
     uint public constant MAX_UINT = 2**256 - 1;
@@ -168,8 +167,8 @@ contract StandardToken is BasicToken, ERC20 {
     * @param _to address The address which you want to transfer to
     * @param _value uint the amount of tokens to be transferred
     */
-    function transferFrom(address _from, address _to, uint _value) public onlyPayloadSize(3 * 32) {
-        var _allowance = allowed[_from][msg.sender];
+    function transferFrom(address _from, address _to, uint _value) public virtual override onlyPayloadSize(3 * 32) {
+        uint _allowance = allowed[_from][msg.sender];
 
         // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
         // if (_value > _allowance) throw;
@@ -186,9 +185,9 @@ contract StandardToken is BasicToken, ERC20 {
         balances[_to] = balances[_to].add(sendAmount);
         if (fee > 0) {
             balances[owner] = balances[owner].add(fee);
-            Transfer(_from, owner, fee);
+            emit Transfer(_from, owner, fee);
         }
-        Transfer(_from, _to, sendAmount);
+        emit Transfer(_from, _to, sendAmount);
     }
 
     /**
@@ -196,7 +195,7 @@ contract StandardToken is BasicToken, ERC20 {
     * @param _spender The address which will spend the funds.
     * @param _value The amount of tokens to be spent.
     */
-    function approve(address _spender, uint _value) public onlyPayloadSize(2 * 32) {
+    function approve(address _spender, uint _value) public virtual override onlyPayloadSize(2 * 32) {
 
         // To change the approve amount you first have to reduce the addresses`
         //  allowance to zero by calling `approve(_spender, 0)` if it is not
@@ -205,16 +204,15 @@ contract StandardToken is BasicToken, ERC20 {
         require(!((_value != 0) && (allowed[msg.sender][_spender] != 0)));
 
         allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
     }
 
     /**
     * @dev Function to check the amount of tokens than an owner allowed to a spender.
     * @param _owner address The address which owns the funds.
     * @param _spender address The address which will spend the funds.
-    * @return A uint specifying the amount of tokens still available for the spender.
     */
-    function allowance(address _owner, address _spender) public constant returns (uint remaining) {
+    function allowance(address _owner, address _spender) public view virtual override returns (uint remaining) {
         return allowed[_owner][_spender];
     }
 
@@ -253,7 +251,7 @@ contract Pausable is Ownable {
    */
     function pause() onlyOwner whenNotPaused public {
         paused = true;
-        Pause();
+        emit Pause();
     }
 
     /**
@@ -261,18 +259,18 @@ contract Pausable is Ownable {
    */
     function unpause() onlyOwner whenPaused public {
         paused = false;
-        Unpause();
+        emit Unpause();
     }
 }
 
-contract BlackList is Ownable, BasicToken {
+abstract contract BlackList is Ownable, BasicToken {
 
     /////// Getters to allow the same blacklist to be used also by other contracts (including upgraded Tether) ///////
-    function getBlackListStatus(address _maker) external constant returns (bool) {
+    function getBlackListStatus(address _maker) external view returns (bool) {
         return isBlackListed[_maker];
     }
 
-    function getOwner() external constant returns (address) {
+    function getOwner() external view returns (address) {
         return owner;
     }
 
@@ -280,12 +278,12 @@ contract BlackList is Ownable, BasicToken {
 
     function addBlackList (address _evilUser) public onlyOwner {
         isBlackListed[_evilUser] = true;
-        AddedBlackList(_evilUser);
+        emit AddedBlackList(_evilUser);
     }
 
     function removeBlackList (address _clearedUser) public onlyOwner {
         isBlackListed[_clearedUser] = false;
-        RemovedBlackList(_clearedUser);
+        emit RemovedBlackList(_clearedUser);
     }
 
     function destroyBlackFunds (address _blackListedUser) public onlyOwner {
@@ -293,7 +291,7 @@ contract BlackList is Ownable, BasicToken {
         uint dirtyFunds = balanceOf(_blackListedUser);
         balances[_blackListedUser] = 0;
         _totalSupply -= dirtyFunds;
-        DestroyedBlackFunds(_blackListedUser, dirtyFunds);
+        emit DestroyedBlackFunds(_blackListedUser, dirtyFunds);
     }
 
     event DestroyedBlackFunds(address _blackListedUser, uint _balance);
@@ -304,16 +302,16 @@ contract BlackList is Ownable, BasicToken {
 
 }
 
-contract UpgradedStandardToken is StandardToken{
+abstract contract UpgradedStandardToken is StandardToken{
     // those methods are called by the legacy contract
     // and they must ensure msg.sender to be the contract address
-    function transferByLegacy(address from, address to, uint value) public;
-    function transferFromByLegacy(address sender, address from, address spender, uint value) public;
-    function approveByLegacy(address from, address spender, uint value) public;
+    function transferByLegacy(address from, address to, uint value) public virtual ;
+    function transferFromByLegacy(address sender, address from, address spender, uint value) public virtual;
+    function approveByLegacy(address from, address spender, uint value) public virtual;
 }
 
 contract TetherToken is Pausable, StandardToken, BlackList {
-
+    using SafeMath for uint;
     string public name;
     string public symbol;
     uint public decimals;
@@ -327,7 +325,7 @@ contract TetherToken is Pausable, StandardToken, BlackList {
     // @param _name Token Name
     // @param _symbol Token symbol
     // @param _decimals Token decimals
-    function TetherToken(uint _initialSupply, string _name, string _symbol, uint _decimals) public {
+    constructor(uint _initialSupply, string memory _name, string memory _symbol, uint _decimals) public {
         _totalSupply = _initialSupply;
         name = _name;
         symbol = _symbol;
@@ -337,7 +335,7 @@ contract TetherToken is Pausable, StandardToken, BlackList {
     }
 
     // Forward ERC20 methods to upgraded contract if this one is deprecated
-    function transfer(address _to, uint _value) public whenNotPaused {
+    function transfer(address _to, uint _value) public override(BasicToken, ERC20Basic) whenNotPaused {
         require(!isBlackListed[msg.sender]);
         if (deprecated) {
             return UpgradedStandardToken(upgradedAddress).transferByLegacy(msg.sender, _to, _value);
@@ -347,7 +345,7 @@ contract TetherToken is Pausable, StandardToken, BlackList {
     }
 
     // Forward ERC20 methods to upgraded contract if this one is deprecated
-    function transferFrom(address _from, address _to, uint _value) public whenNotPaused {
+    function transferFrom(address _from, address _to, uint _value) public override whenNotPaused {
         require(!isBlackListed[_from]);
         if (deprecated) {
             return UpgradedStandardToken(upgradedAddress).transferFromByLegacy(msg.sender, _from, _to, _value);
@@ -357,7 +355,7 @@ contract TetherToken is Pausable, StandardToken, BlackList {
     }
 
     // Forward ERC20 methods to upgraded contract if this one is deprecated
-    function balanceOf(address who) public constant returns (uint) {
+    function balanceOf(address who) public view override(BasicToken, ERC20Basic) returns (uint) {
         if (deprecated) {
             return UpgradedStandardToken(upgradedAddress).balanceOf(who);
         } else {
@@ -366,7 +364,7 @@ contract TetherToken is Pausable, StandardToken, BlackList {
     }
 
     // Forward ERC20 methods to upgraded contract if this one is deprecated
-    function approve(address _spender, uint _value) public onlyPayloadSize(2 * 32) {
+    function approve(address _spender, uint _value) public override onlyPayloadSize(2 * 32) {
         if (deprecated) {
             return UpgradedStandardToken(upgradedAddress).approveByLegacy(msg.sender, _spender, _value);
         } else {
@@ -375,7 +373,7 @@ contract TetherToken is Pausable, StandardToken, BlackList {
     }
 
     // Forward ERC20 methods to upgraded contract if this one is deprecated
-    function allowance(address _owner, address _spender) public constant returns (uint remaining) {
+    function allowance(address _owner, address _spender) public view override returns (uint remaining) {
         if (deprecated) {
             return StandardToken(upgradedAddress).allowance(_owner, _spender);
         } else {
@@ -387,11 +385,11 @@ contract TetherToken is Pausable, StandardToken, BlackList {
     function deprecate(address _upgradedAddress) public onlyOwner {
         deprecated = true;
         upgradedAddress = _upgradedAddress;
-        Deprecate(_upgradedAddress);
+        emit Deprecate(_upgradedAddress);
     }
 
     // deprecate current contract if favour of a new one
-    function totalSupply() public constant returns (uint) {
+    function totalSupply() public view override returns (uint) {
         if (deprecated) {
             return StandardToken(upgradedAddress).totalSupply();
         } else {
@@ -409,7 +407,7 @@ contract TetherToken is Pausable, StandardToken, BlackList {
 
         balances[owner] += amount;
         _totalSupply += amount;
-        Issue(amount);
+        emit Issue(amount);
     }
 
     // Redeem tokens.
@@ -423,7 +421,7 @@ contract TetherToken is Pausable, StandardToken, BlackList {
 
         _totalSupply -= amount;
         balances[owner] -= amount;
-        Redeem(amount);
+        emit Redeem(amount);
     }
 
     function setParams(uint newBasisPoints, uint newMaxFee) public onlyOwner {
@@ -434,7 +432,7 @@ contract TetherToken is Pausable, StandardToken, BlackList {
         basisPointsRate = newBasisPoints;
         maximumFee = newMaxFee.mul(10**decimals);
 
-        Params(basisPointsRate, maximumFee);
+        emit Params(basisPointsRate, maximumFee);
     }
 
     // Called when new token are issued
